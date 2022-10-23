393a5cc7b2a841e01ef58ede2f9bdecb18b23a0a
osawa-koki <osawa-koki@osawa.dev>
2022-10-23 06:16:25 +0000

    (´;ω;｀)

--- src/server.go
+++ src/server.go
@@ -1,8 +1,3 @@
-/*----------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See LICENSE in the project root for license information.
- *---------------------------------------------------------------------------------------*/
-
 package main
 
 import (
@@ -21,3 +16,4 @@ func main() {
   fmt.Println("Server listening on port ", portNumber)
   http.ListenAndServe(":"+portNumber, nil)
 }
+
